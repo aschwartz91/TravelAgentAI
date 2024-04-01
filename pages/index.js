0b38ac326963d799e52c084bd49b4768b78d60a0
aschwartz91 <77259180+aschwartz91@users.noreply.github.com>
2024-04-01 14:43:38 -0500

    AI Travel Agent

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
-import Head from "next/head";
+/*
+A. Schwartz
+4/18/23
+
+Much of this project setup comes from OpenAI's API documentation and template guidelines.
+View this link to find the source for the API: https://openai.com/blog/openai-api
+*/
+
 import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [tripInput, setTripInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +21,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ trip: tripInput }),
       });
 
       const data = await response.json();
@@ -23,9 +30,8 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setTripInput("");
     } catch(error) {
-      // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
@@ -33,23 +39,20 @@ export default function Home() {
 
   return (
     <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+      <header>
+        <title>Virtual Travel Agent</title>
+        <link rel="icon" href="/plane.png" />
+      </header>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="/plane.png" className={styles.icon} />
+        <h3>Virtual Travel Agent</h3>
+        
         <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
+        Enter your destination, the number of days and any special activities you'd like to do separated with semicolons:
+        
+          <input type="text" name="trip" placeholder="i.e. Tokyo; 3 days; biking" value={tripInput} onChange={(e) => setTripInput(e.target.value)}/>
+          <input type="submit" value="Generate Itinerary"/>
         </form>
         <div className={styles.result}>{result}</div>
       </main>
